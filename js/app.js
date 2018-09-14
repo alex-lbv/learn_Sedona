b403b725b4536ac92cf1c1fd9f9231bf821168d3
Alexandr <aal940722@gmail.com>
2018-09-15 01:24:21 +0300

    Попытка_№2

---
+++ js/app.js
@@ -0,0 +1,24 @@
+function hideForm(form) {
+    form.style.height = 0;
+    form.style.padding = 0;
+    form.classList.remove("is-opened");
+}
+function showForm(form, height) {
+    form.style.height = height + "px";
+    form.style.padding = null;
+    form.classList.add("is-opened");
+}
+(function() {
+  const button = document.getElementsByClassName("j-catalog-button")[0];
+  const form = document.getElementsByClassName("j-search-form")[0];
+  const formPaddingTop = 55;
+  const formHeight = form.clientHeight - formPaddingTop;
+  hideForm(form);
+  button.onclick = function() {
+    if (form.classList.contains("is-opened")) {
+      hideForm(form);
+    } else {
+      showForm(form, formHeight);
+    }
+  }
+})();
